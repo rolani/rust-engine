d18347b373753996dac519af64f823677a22a6bc
Richard Olaniyan <34588531+rolani@users.noreply.github.com>
2023-09-10 03:04:17 +0000

    Update README.md

---
+++ webapp/src/lib.rs
@@ -0,0 +1,21 @@
+// build webapp functions
+
+// multiply two integers
+pub fn mult(n1: i32, n2: i32) -> i32 {
+    n1 * n2
+}
+
+// multiply two integers
+pub fn add(n1: i32, n2: i32) -> i32 {
+    n1 + n2
+}
+
+// multiply two integers
+pub fn sub(n1: i32, n2: i32) -> i32 {
+    n1 - n2
+}
+
+// divide two integers
+pub fn div(n1: i32, n2: i32) -> i32 {
+    n1 / n2
+}
